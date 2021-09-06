acdb20667b5ed4d3d31e6986206680e1829576b8
Frode Sjovatsen <frode@fintlabs.no>
2021-09-06 13:18:15 +0200

    FIN-20 #comment inital commit #in-progress

---
+++ src/main/java/no/vigoiks/vigoauthorganisationselector/VigoAuthOrganisationSelectorApplication.java
@@ -0,0 +1,13 @@
+package no.vigoiks.vigoauthorganisationselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigoAuthOrganisationSelectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VigoAuthOrganisationSelectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/vigoiks/vigoauthorganisationselector/VigoAuthOrganisationSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.vigoiks.vigoauthorganisationselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigoAuthOrganisationSelectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
