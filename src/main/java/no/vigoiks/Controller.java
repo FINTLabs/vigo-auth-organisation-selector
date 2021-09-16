ac77ba8df19887e3669e1a021200f65f2982e4d7
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2021-09-16 21:02:59 +0200

    Fin 47 redirect from backend (#4)
    * FIN-47 #comment Added support for redirecting from backend and metrics #in-progress
    
    * FIN-48 #comment Added support for target query parameter #in-progress
    
    * vrææææææææl

--- src/main/java/no/vigoiks/Controller.java
+++ src/main/java/no/vigoiks/Controller.java
@@ -1,7 +1,6 @@
 package no.vigoiks;
 
 import io.micrometer.core.instrument.MeterRegistry;
-
 import lombok.extern.slf4j.Slf4j;
 import no.vigoiks.config.AppConfig;
 import no.vigoiks.model.AuthenticationContract;
@@ -11,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
-
-import java.net.URLClassLoader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -46,7 +45,6 @@ public class Controller {
     public ResponseEntity<RedirectResponse> redirect(@RequestBody RedirectProperties redirectProperties) {
         meterRegistry.counter("vigo.common.auth.contract.redirect", "id", redirectProperties.getId(), "target", redirectProperties.getTarget()).increment();
         RedirectResponse redirectResponse = new RedirectResponse();
-
         UriComponentsBuilder url = UriComponentsBuilder.newInstance()
                 .scheme("https")
                 .host("idp.felleskomponent.no")
@@ -57,7 +55,7 @@ public class Controller {
         }
 
         if (StringUtils.hasText(redirectProperties.getTarget())) {
-            url.queryParam("target", redirectProperties.getTarget());
+            url.queryParam("target", encodeValue(redirectProperties.getTarget()));
         }
 
         if (StringUtils.hasText(redirectProperties.getSid())) {
@@ -65,8 +63,16 @@ public class Controller {
         }
 
         redirectResponse.setUrl(url.build().toUriString());
-
         return ResponseEntity.ok(redirectResponse);
+    }
+
+    private String encodeValue(String value) {
+        try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            log.info("Shitt");
+            return value;
+        }
     }
 
 }
